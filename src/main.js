b3cbcceadb22c2d312db6e5d83b171dfae662dfe
kevin.chu <kevin.chu@gordon.edu>
2022-01-31 13:20:30 -0500

    Add Main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import styles from './main.style.js';
+'use strict';
+
+const Main = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Team Project</Text>
+        </View>
+    )
+}
+
+export default Main;
--- src/main.style.js
+++ src/main.style.js
@@ -2,14 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-    },
-    statusbarNormal: STATUSBAR_HEIGHT => ({
-        paddingTop: STATUSBAR_HEIGHT,
-    }),
-    statusbarHidden: {
-        paddingTop: 0,
     },
 })
