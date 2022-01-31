a303b761b69efc7f9a48f60805e835f2a5fcbc51
kevin.chu <kevin.chu@gordon.edu>
2022-01-31 16:38:05 -0500

    reordered folder hierarchy

--- TeamProject/App.js
+++ TeamProject/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, NativeModules, View, Text, Button } from 'react-native';
-import styles from './src/main.style.js';
+import { View, Text, Button } from 'react-native';
 import Main from './src/main.js';
 
 const App = () => {
