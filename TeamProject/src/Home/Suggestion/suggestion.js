5c1f9b6205b09cc6928f6abff3da263d8556a49c
kevin.chu <kevin.chu@gordon.edu>
2022-02-22 11:36:52 -0500

    added: basic suggestion

--- TeamProject/src/Home/Suggestion/suggestion.js
+++ TeamProject/src/Home/Suggestion/suggestion.js
@@ -1,16 +1,58 @@
-import { View, ImageBackground } from 'react-native';
+import React from 'react';
+import { View, ScrollView, Text } from 'react-native';
 import styles from './suggestion.style.js';
+import MyPlants from './../../Garden/myPlant.js';
+
 'use strict';
 
-const Suggestion = () => {
+class Suggestion extends React.Component {
 
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            garden: [],
+        }
+    }
 
-    return (
-        <View style={styles.container}>
-            
-        </View>
-    )
+    handleCallback(childData) {
+        
+        this.setState({garden: childData});
+        
+    }
+
+    renderPlants() {
+        // console.log(this.state.plant);
+        return (
+            this.state.garden.map((plant) => (
+                <View style={{ height: 100, flexDirection: 'row', }}>
+                    <View style={styles.plantState}>
+                        <Text>Hello</Text>
+                    </View>
+                </View>
+            ))
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MyPlants parentCallBack={this.handleCallback} />
+                <ScrollView 
+                    contentContainerStyle={{ flexGrow: 1 }}
+                    horizontal={false}
+                    style={{ paddingHorizontal: 10, flex: 10 }}
+                >
+                    {this.renderPlants()}
+                    <View style={{ height: 100, flexDirection: 'row', }}>
+                        <View style={styles.plantState}>
+                        </View>
+                    </View>
+                </ScrollView>
+                
+            </View>
+        )
+    }
 }
 
 export default Suggestion;
